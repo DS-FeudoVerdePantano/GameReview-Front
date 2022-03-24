1bcb88e7435b8569bd4e052cb62a73136c533879
Guilherme Costa <guilhermercosta1@gmail.com>
2022-03-24 11:54:35 -0300

    Folder Images -> Assets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //import './App.css';
 import './Normilaze.css'
 import Homepage from './Pages/homepage';
-import capa from './Images/Capa the witcher 3.png';
+import capa from './Assets/Capa the witcher 3.png';
 import './Components/scrollbar/style.css';
 
 function App() {
