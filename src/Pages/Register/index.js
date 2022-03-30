e8af1499ba6d1fb0f8957e1dccadf9a4800def0a
VCOliver <victorcruz.01@gmail.com>
2022-03-30 19:36:03 -0300

    Register page connected to backend

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-// import { useNavigate } from 'react-router-dom'
-// import api from '../../services/api';
+import { useNavigate } from 'react-router-dom'
+import api from '../../services/api';
 import './style.css';
 import brasao from '../../Images/brasaoleao.png'
 
@@ -10,22 +10,44 @@ function Register() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [password2, setPassword2] = useState('')
+  const [passwordsMatch, setPasswordsMatch] = useState(true)
   const [success, setSuccess] = useState(true)
 
-  const CheckPassword=(event)=>{
+  const redirect = useNavigate()
+
+  async function handleForm(event){
     event.preventDefault();
 
     if(password !== password2){
-      setSuccess (false);
+      setPasswordsMatch(false);
+    } else {
+
+      setPasswordsMatch(true);
+
+      await api.post('/auth/register', {
+        name: username,
+        email: email,
+        password: password,
+      }).then(response => {
+        localStorage.setItem('user', response.data.user._id);
+        localStorage.setItem('token', response.data.token);
+        console.log(response);
+        redirect('/')
+        window.location.reload();
+      }).catch(error => {
+        setSuccess(false);
+      });   
+
     }
+
   }
 
   return (
     <div className="Tudo">
       <div className="CaixaRegistro">
         <h1 className="tituloRegistro">Registro</h1>
-        <form autocomplete="off"
-        onSubmit={CheckPassword}
+        <form autoComplete="off"
+        onSubmit={handleForm}
         >
           <input
             type='text'
@@ -59,11 +81,12 @@ function Register() {
             onChange={event => setPassword2(event.target.value)}
             placeholder='Confirm password'
           />
+          {passwordsMatch ? null : <p className='inputError'>As senhas estão diferentes</p>}
           {
           success ?
             null
             :
-            <p className='inputError'>As senhas estão diferentes</p>
+            <p className='inputError'>Usuário já Existe</p>
           }
           <button className='register-button' type='submit'>Registro</button>
         </form>
