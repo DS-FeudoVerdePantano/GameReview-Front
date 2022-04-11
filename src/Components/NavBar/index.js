910a5c3c0ce815249b11951adcbc79d6ccdae363
Joao Pedro <joaopedrobsb3@gmail.com>
2022-04-11 11:58:09 -0300

    Search bar appear to be working, no game page implemented yet, awaiting further testing

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,10 +1,32 @@
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import brasao from './../../Images/brasaoleao.png';
 import menu from './../../Images/Sanduiche.svg';
 import perfil from './../../Images/Perfil.svg';
 import './style.css';
+import Results from './searchResults';
 
 function Navbar(){
+
+    const [searchTerm, setSearchTerm] = useState("")
+    const [gameResults, setGameResults] = useState([])
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        let comp = searchTerm.split(' ').join('-').toLowerCase()
+    
+        setGameResults([])
+        fetch(`https://rawg.io/api/games?key=c08f80574bca406bbcf96b7e452b3e91?search=${comp}`)
+        .then(res => res.json())
+        .then(({results}) => {
+            results === undefined ? alert('no games found') : setGameResults(results)
+        })
+        setSearchTerm("")
+    }
 
     const redirect = useNavigate()
 
@@ -24,7 +46,12 @@ function Navbar(){
 
                     <div className="right-box">
                         <div className="rb-input">
-                            <input type="text"  placeholder="Pesquisar.."  />
+                        <form onSubmit={onSubmit}>
+                            <input type="text" value={searchTerm} onChange={handleChange}/>
+                            <br></br>
+                            <input type="submit"/>
+                        </form>
+                            <Results gameResults={gameResults} />
                         </div>
 
                         <div className="rb-perfil">
